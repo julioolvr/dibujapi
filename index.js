3eb7ae48087c87bc7431f22ae8077e7a7a5201a9
Julio Olivera <julio.olvr@gmail.com>
2017-09-03 21:57:55 -0300

    CORS support

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 
 const imageSearch = require("./services/googleImageSearch");
 
+// TODO: Get origins from config
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://localhost:9009"]
+};
+
 const app = express();
+app.use(cors(corsOptions));
 
 app.get("/search", function(req, res) {
   imageSearch(req.query.q).then(results => res.send(results));
